2ca52e39de20ef1a39ec90b19143cc91502e6266
奏 <miyachikozin@gmail.com>
2025-08-03 18:59:00 +0900

    新しいロジックを追加

--- main.js
+++ main.js
@@ -12,6 +12,22 @@ function createWindow() {
   });
 
   win.loadFile('index.html');
+
+  
+  win.webContents.on('new-window', (event, url) => {
+    event.preventDefault();
+    if (url === 'file://' + path.join(__dirname, 'settings.html')) {
+      const settingsWin = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false,
+        },
+      });
+      settingsWin.loadFile('settings.html');
+    }
+  });
 }
 
 app.whenReady().then(() => {
